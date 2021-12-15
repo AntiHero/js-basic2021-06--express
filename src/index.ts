302e0fd676002101f999b262f978b1b787df09de
Aleksandr Schemelev <prophreaker@gmail.com>
2021-12-15 21:49:37 +0300

    feat: add express-session + test

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,39 @@
 import express from 'express';
 import { resolve } from 'path';
-
-
+import requetLogger from './middlewares/logger';
+import bodyParser from 'body-parser';
+import session from 'express-session';
+import './types/index';
 
 const app = express();
 
+const oneDay = 1000 * 60 * 60 * 24;
+
+// Подключение
+app.use(
+  session({
+    secret: "sfajnh4faAN99", // обязательное поле
+    cookie: { maxAge: oneDay },
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(bodyParser.json());
+app.use(requetLogger);
 app.use(express.static('public'));
+
+app.post('/api/counter', (req, res) => {
+  const { value } = req.body;
+  
+  if (!req.session.counter) {
+    req.session.counter = 1;
+  } else {
+    req.session.counter += value;
+  }
+
+  res.status(201).send(String(req.session.counter));
+});
 
 app.get('*/style.css', (req, res) => {
   res.status(200).sendFile(resolve(__dirname, '../public/style.css'));
